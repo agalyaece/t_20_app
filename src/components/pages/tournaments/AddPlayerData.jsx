7bd28c23fbb696568653b14cf018b142ce233375
agalyaece <agalyapytest@gmail.com>
2024-08-28 19:59:12 +0530

    initial commit

--- src/components/pages/tournaments/AddPlayerData.jsx
+++ src/components/pages/tournaments/AddPlayerData.jsx
@@ -54,6 +54,14 @@ export default function AddPlayerData() {
     });
 
     return <>
+     <Link to={`/cricket/icc_world_cup`}>
+        <button type="button" className="w-full flex items-center justify-center m-20 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-slate-200 border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+          <svg className="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+          </svg>
+          <span>Go back</span>
+        </button>
+      </Link>
         {isLoading ? (
             <p>Loading...</p>
         ) : error ? (
